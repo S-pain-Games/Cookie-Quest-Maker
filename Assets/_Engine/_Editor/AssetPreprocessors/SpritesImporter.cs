8c1ec9fecf5c7d27c56ff8204872a8a2ea7d2c37
Anthony <62327779+Anthonys-source@users.noreply.github.com>
2021-10-11 18:28:51 +0200

    Add Bakery Mockup

--- Assets/_Engine/_Editor/AssetPreprocessors/SpritesImporter.cs
+++ Assets/_Engine/_Editor/AssetPreprocessors/SpritesImporter.cs
@@ -8,7 +8,7 @@ public class SpritesImporter : AssetPostprocessor
     void OnPostprocessSprites(Texture2D texture, Sprite[] sprites)
     {
         TextureImporter textureImporter = assetImporter as TextureImporter;
-        //textureImporter.spritePixelsPerUnit = 32;
+        textureImporter.spritePixelsPerUnit = 64;
         textureImporter.filterMode = FilterMode.Point;
         textureImporter.wrapMode = TextureWrapMode.Clamp;
         var importSettings = textureImporter.GetDefaultPlatformTextureSettings();
