0d5cf0d50620fd53f75852883e2192bf3e100afd
Anthony <62327779+Anthonys-source@users.noreply.github.com>
2021-10-14 12:11:29 +0200

    Add ToList method to Phrase

--- Assets/_Scripts/QuestMaking/PhraseBuilderBehaviour.cs
+++ Assets/_Scripts/QuestMaking/PhraseBuilderBehaviour.cs
@@ -23,17 +23,38 @@ public class PhraseBuilderBehaviour : MonoBehaviour
     }
 }
 
+[System.Serializable]
 public struct Phrase
 {
-    public Word Action { get => m_Action; }
-    public Word Target { get => m_Target; }
+    public Word Modifier => m_Modifier;
+    public Word Action => m_Action;
+    public Word Target => m_Target;
+    public Word Object => m_Object;
 
+    private Word m_Modifier;
     private Word m_Action;
     private Word m_Target;
+    private Word m_Object;
 
     public Phrase(Word action, Word target)
     {
+        m_Modifier = null;
         m_Action = action;
         m_Target = target;
+        m_Object = null;
+    }
+
+    public List<Word> ToList()
+    {
+        List<Word> words = new List<Word>();
+
+        // These null checks on unity objects are really expensive
+        // We should use a bitmask
+        if (m_Action != null)
+            words.Add(m_Action);
+        if (m_Target != null)
+            words.Add(m_Target);
+
+        return words;
     }
 }
