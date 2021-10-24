98fe4efd6b80eeedd30586c06332ae0da492f0e2
Fernando <62439791+Kreithor@users.noreply.github.com>
2021-10-24 19:47:54 +0200

    Added first version of dialogue boxes

--- Assets/_Scripts/Dialogues/Dialogue.cs
+++ Assets/_Scripts/Dialogues/Dialogue.cs
@@ -9,12 +9,36 @@ public class Dialogue : MonoBehaviour
     private TextMeshProUGUI CharacterName;
 
     [SerializeField]
-    private TextMeshProUGUI text;
+    private TextMeshProUGUI textDialogue;
 
     [SerializeField]
     private string[] sentences;
 
     private string ID_Dialogue;
 
-    private int indexText;
+    private int indexText = 0;
+
+    public void showDialogue(string[] sentencesToShow, string name)
+    {
+        sentences = sentencesToShow;
+        CharacterName.text = name;
+
+        textDialogue.text = string.Empty;
+        textDialogue.text = sentences[indexText];
+    }
+
+    public void nextLine()
+    {
+        if (indexText < sentences.Length - 1)
+        {
+            textDialogue.text = string.Empty;
+            indexText++;
+            textDialogue.text = sentences[indexText];
+        }
+        else
+        {
+            gameObject.SetActive(false);
+            indexText = 0;
+        }
+    }
 }
