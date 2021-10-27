8bcb32225895dfc37bdfff5e32f1e89682257b93
Anthonys-source <62327779+Anthonys-source@users.noreply.github.com>
2021-10-27 22:55:36 +0200

    Fix Asset Import and Clean Up Scene

--- Assets/_Engine/_Editor/AssetPreprocessors/SpritesImporter.cs
+++ Assets/_Engine/_Editor/AssetPreprocessors/SpritesImporter.cs
@@ -5,14 +5,14 @@ using UnityEditor;
 
 public class SpritesImporter : AssetPostprocessor
 {
-    void OnPostprocessSprites(Texture2D texture, Sprite[] sprites)
-    {
-        TextureImporter textureImporter = assetImporter as TextureImporter;
-        textureImporter.spritePixelsPerUnit = 64;
-        textureImporter.filterMode = FilterMode.Point;
-        textureImporter.wrapMode = TextureWrapMode.Clamp;
-        var importSettings = textureImporter.GetDefaultPlatformTextureSettings();
-        importSettings.format = TextureImporterFormat.RGBA32;
-        textureImporter.SetPlatformTextureSettings(importSettings);
-    }
+    //void OnPostprocessSprites(Texture2D texture, Sprite[] sprites)
+    //{
+    //    TextureImporter textureImporter = assetImporter as TextureImporter;
+    //    textureImporter.spritePixelsPerUnit = 64;
+    //    textureImporter.filterMode = FilterMode.Point;
+    //    textureImporter.wrapMode = TextureWrapMode.Clamp;
+    //    var importSettings = textureImporter.GetDefaultPlatformTextureSettings();
+    //    importSettings.format = TextureImporterFormat.RGBA32;
+    //    textureImporter.SetPlatformTextureSettings(importSettings);
+    //}
 }
