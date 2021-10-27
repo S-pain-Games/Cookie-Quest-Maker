b88619daa14ee8f28a4b291624d9e0eaa5cd56f3
Anthonys-source <62327779+Anthonys-source@users.noreply.github.com>
2021-10-27 17:26:16 +0200

    Reorder files and mark deprecated scripts

--- Assets/_Scripts/Town's State/StateManager.cs
+++ Assets/_Scripts/Town's State/StateManager.cs
@@ -2,6 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * 
+ * DEPRECATED
+ * 
+ */
+
 public class StateManager : MonoBehaviour
 {
     [SerializeField]
