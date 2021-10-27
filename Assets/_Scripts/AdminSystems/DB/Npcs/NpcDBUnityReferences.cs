42cf12d5e498b43f2ae694d894d385ef7abdf154
Anthonys-source <62327779+Anthonys-source@users.noreply.github.com>
2021-10-27 18:02:34 +0200

    Add QOL Editor changes to NPC unity ref

--- Assets/_Scripts/AdminSystems/DB/Npcs/NpcDBUnityReferences.cs
+++ Assets/_Scripts/AdminSystems/DB/Npcs/NpcDBUnityReferences.cs
@@ -1,9 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class NpcDBUnityReferences : MonoBehaviour
 {
     // this class is just for consistency in the systems design
     public List<NPCBehaviour> m_NpcBehaviour = new List<NPCBehaviour>();
+
+    private void Awake()
+    {
+        // This is quite expensive an unnecesary but it temporarily fixes problems
+        m_NpcBehaviour = FindObjectsOfType<NPCBehaviour>(true).ToList();
+
+#if UNITY_EDITOR
+        if (m_NpcBehaviour.Count == 0)
+        {
+            Debug.LogError("No npcs found for the NPC System");
+        }
+#endif
+    }
 }
