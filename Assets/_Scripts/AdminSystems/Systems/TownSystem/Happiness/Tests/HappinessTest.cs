19be77bed01c5dc3dfa74f4d28f21a7c499b5b80
Anthonys-source <62327779+Anthonys-source@users.noreply.github.com>
2021-10-30 14:34:57 +0200

    Remove unused editor test assemblies

--- Assets/_Scripts/AdminSystems/Systems/TownSystem/Happiness/Tests/HappinessTest.cs
+++ Assets/_Scripts/AdminSystems/Systems/TownSystem/Happiness/Tests/HappinessTest.cs
@@ -1,27 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
-using NUnit.Framework;
-using UnityEngine;
-using UnityEngine.TestTools;
+//using System.Collections;
+//using System.Collections.Generic;
+//using NUnit.Framework;
+//using UnityEngine;
+//using UnityEngine.TestTools;
 
 
-public class HappinessTest
-{
-    private Happiness _happiness;
+//public class HappinessTest
+//{
+//    private Happiness _happiness;
 
-    [SetUp]
-    public void SetUpTest()
-    {
-        GameObject h = new GameObject();
-        _happiness = h.AddComponent<Happiness>();
-    }
+//    [SetUp]
+//    public void SetUpTest()
+//    {
+//        GameObject h = new GameObject();
+//        _happiness = h.AddComponent<Happiness>();
+//    }
 
-    [Test]
-    public void TestHappiness()
-    {
-        int randomHappiness = Random.Range(-100, 100);
-        _happiness.pointsHappiness(randomHappiness);
-        Debug.Log("Happiness to add: " + randomHappiness);
-        Debug.Log("New happiness: " + _happiness.currentHappiness);
-    }
-}
+//    [Test]
+//    public void TestHappiness()
+//    {
+//        int randomHappiness = Random.Range(-100, 100);
+//        _happiness.pointsHappiness(randomHappiness);
+//        Debug.Log("Happiness to add: " + randomHappiness);
+//        Debug.Log("New happiness: " + _happiness.currentHappiness);
+//    }
+//}
