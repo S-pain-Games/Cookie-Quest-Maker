a16c82f4b768015b654e58a5bc5380b98a843171
Anthonys-source <62327779+Anthonys-source@users.noreply.github.com>
2021-11-19 18:06:51 +0100

    Tutorial Sequence Demo

--- Assets/_Scripts/AdminSystems/Systems/TutorialSystem/FirstDayDeitiesScriptedSequence.cs
+++ Assets/_Scripts/AdminSystems/Systems/TutorialSystem/FirstDayDeitiesScriptedSequence.cs
@@ -36,32 +36,126 @@ public class FirstDayDeitiesScriptedSequence : MonoBehaviour
         nuRef = Instantiate(nuPrefab, nuSpawnPos);
         _disableMovementCmd.Invoke();
         _toggleGameplayUiCmd.Invoke();
-        StartDialogue();
+        StartNuEvithIntroSequence();
     }
 
-    private void StartDialogue()
+    #region Dialogue Sequence
+    public void StartNuEvithIntroSequence()
     {
-        ShowDialogueEvtArgs d = new ShowDialogueEvtArgs(new List<string>() { "Dialogue1" }, new ID("narrator"), ShowDialogue_2);
-        _showDialogueCmd.Invoke(d);
+        _showDialogueCmd.Invoke(new ShowDialogueEvtArgs(new List<string>() {
+            "¡Vaya, vaya, bonita pastelería tienes aquí montada, jovenzuelo!"},
+         new ID("evith"),
+            () => { this.EvithNuIntroSequenceTwo(); }));
+    }
+    private void EvithNuIntroSequenceTwo()
+    {
+        _showDialogueCmd.Invoke(new ShowDialogueEvtArgs(new List<string>() {
+            "Permíteme presentarme. Soy Nu, Ángel Patrón de las Galletas y guía de los habitantes de este mundo. Y mi impresentable compañera es..."},
+        new ID("nu"),
+           () => { this.EvithNuIntroSequenceThree(); }));
+    }
+    private void EvithNuIntroSequenceThree()
+    {
+        _showDialogueCmd.Invoke(new ShowDialogueEvtArgs(new List<string>() {
+            "¡Para tí soy la majestuosa Evith, Reina del Caos Eterno y la auténtica Patrona de las Galletas! ¡No te dejes engañar por este santurrón presuntuoso!"},
+        new ID("evith"),
+           () => { this.EvithNuIntroSequenceFour(); }));
+    }
+    private void EvithNuIntroSequenceFour()
+    {
+        _showDialogueCmd.Invoke(new ShowDialogueEvtArgs(new List<string>() {
+            "¡No es momento de discutir! Recuerda nuestro propósito en este lugar."},
+       new ID("nu"),
+          () => { EvithNuIntroSequenceFive(); }));
+    }
+    private void EvithNuIntroSequenceFive()
+    {
+        _showDialogueCmd.Invoke(new ShowDialogueEvtArgs(new List<string>() {
+            "¿Nuestro propósito? ¡Ah, sí! Hemos escuchado tu deseo."},
+       new ID("evith"),
+          () => { EvithNuIntroSequenceSix(); }));
+    }
+    private void EvithNuIntroSequenceSix()
+    {
+        _showDialogueCmd.Invoke(new ShowDialogueEvtArgs(new List<string>() {
+            "No quieres seguir escuchando los problemas de la gente sin poder hacer nada al respecto."},
+      new ID("nu"),
+         () => { EvithNuIntroSequenceSeven(); }));
+    }
+    private void EvithNuIntroSequenceSeven()
+    {
+        _showDialogueCmd.Invoke(new ShowDialogueEvtArgs(new List<string>() {
+            "¿Quieres hacer algo por el pueblo, verdad? ¡Pues nosotros estamos aquí porque hemos decidido darte una oportunidad!"},
+      new ID("evith"),
+         () => { EvithNuIntroSequenceEight(); }));
+    }
+    private void EvithNuIntroSequenceEight()
+    {
+        _showDialogueCmd.Invoke(new ShowDialogueEvtArgs(new List<string>() {
+            "Durante un tiempo limitado, vamos a ofrecerte parte de nuestro poder para que puedas tomar cartas en el asunto."},
+      new ID("nu"),
+         () => { EvithNuIntroSequenceNine(); }));
+    }
+    private void EvithNuIntroSequenceNine()
+    {
+        _showDialogueCmd.Invoke(new ShowDialogueEvtArgs(new List<string>() {
+            "No hay ningún tipo de trampa ni de letra pequeña. Lo hemos decidido así porque hemos visto que tienes potencial."},
+     new ID("evith"),
+        () => { EvithNuIntroSequenceTen(); }));
+    }
+    private void EvithNuIntroSequenceTen()
+    {
+        _showDialogueCmd.Invoke(new ShowDialogueEvtArgs(new List<string>() {
+            "Empecemos pues. Será suficiente con ese horno y la mesa."},
+     new ID("nu"),
+        () => { StartEvithNuPreFurnaceSequence(); }));
     }
 
-    private void ShowDialogue_2()
-    {
-        ShowDialogueEvtArgs d = new ShowDialogueEvtArgs(new List<string>() { "Dialogue1" }, new ID("nu"), ShowDialogue_3);
-        _showDialogueCmd.Invoke(d);
-    }
+    // ========================================================
+    //  NU & EVITH PRE FURNACE SEQUENCE
+    // ========================================================
 
-    private void ShowDialogue_3()
+    private void StartEvithNuPreFurnaceSequence()
     {
-        ShowDialogueEvtArgs d = new ShowDialogueEvtArgs(new List<string>() { "Dialogue1" }, new ID("evith"), ShowDialogue_4);
-        _showDialogueCmd.Invoke(d);
+        _showDialogueCmd.Invoke(new ShowDialogueEvtArgs(new List<string>() {
+            "Esta harina que tienes es muy normalucha. ¡Mejor usa esta Harina Encantada!"},
+        new ID("evith"),
+           () => { this.EvithNuPreFurnaceSequenceTwo(); }));
     }
-
-    private void ShowDialogue_4()
+    private void EvithNuPreFurnaceSequenceTwo()
     {
-        ShowDialogueEvtArgs d = new ShowDialogueEvtArgs(new List<string>() { "Dialogue1" }, new ID("nu"), FinishSequence);
-        _showDialogueCmd.Invoke(d);
+        _showDialogueCmd.Invoke(new ShowDialogueEvtArgs(new List<string>() {
+            "Con esta Harina Encantada podrás hornear Galletas mágicas que te ayudarán en tu meta."},
+       new ID("nu"),
+          () => { EvithNuPreFurnaceSequenceThree(); }));
     }
+    private void EvithNuPreFurnaceSequenceThree()
+    {
+        _showDialogueCmd.Invoke(new ShowDialogueEvtArgs(new List<string>() {
+            "Las Galletas harán cualquier tarea que les asignes. Además no necesitan alimento ni sueldo. ¡Son los sirvientes perfectos!"},
+       new ID("evith"),
+          () => { EvithNuPreFurnaceSequenceFour(); }));
+    }
+    private void EvithNuPreFurnaceSequenceFour()
+    {
+        _showDialogueCmd.Invoke(new ShowDialogueEvtArgs(new List<string>() {
+            "Las Galletas necesitan instrucciones para saber qué deben hacer."},
+      new ID("nu"),
+         () => { EvithNuPreFurnaceSequenceFive(); }));
+    }
+    private void EvithNuPreFurnaceSequenceFive()
+    {
+        _showDialogueCmd.Invoke(new ShowDialogueEvtArgs(new List<string>() {
+            "Pronto cobrará sentido ¡Vamos, ponte delante del horno o de la mesa y nosotros te iremos indicando!"},
+      new ID("evith"),
+         () => { EvithNuPreFurnaceSequenceEnd(); }));
+    }
+    private void EvithNuPreFurnaceSequenceEnd()
+    {
+        //Debug.Log("Se acabó la chapa");
+        FinishSequence();
+    }
+    #endregion
 
     private void FinishSequence()
     {
