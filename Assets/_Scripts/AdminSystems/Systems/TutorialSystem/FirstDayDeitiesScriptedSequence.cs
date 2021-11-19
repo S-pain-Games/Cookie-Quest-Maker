ecc0d6a7c2563ce499ba00ddd8527febb9bf8a53
Anthonys-source <62327779+Anthonys-source@users.noreply.github.com>
2021-11-19 18:33:39 +0100

    Fix Tutorial Sequence Sprites

--- Assets/_Scripts/AdminSystems/Systems/TutorialSystem/FirstDayDeitiesScriptedSequence.cs
+++ Assets/_Scripts/AdminSystems/Systems/TutorialSystem/FirstDayDeitiesScriptedSequence.cs
@@ -34,6 +34,8 @@ public class FirstDayDeitiesScriptedSequence : MonoBehaviour
     {
         evithRef = Instantiate(evithPrefab, evithSpawnPos);
         nuRef = Instantiate(nuPrefab, nuSpawnPos);
+        evithPrefab.GetComponent<SpriteRenderer>().flipX = true;
+        nuRef.GetComponent<SpriteRenderer>().flipX = true;
         _disableMovementCmd.Invoke();
         _toggleGameplayUiCmd.Invoke();
         StartNuEvithIntroSequence();
