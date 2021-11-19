32d54bd46f58618b1f505abcbb97b730f9da18d9
jdrojo2018 <drojomartin@gmail.com>
2021-11-19 17:28:57 +0100

    Added random dialogs to NPCs

--- Assets/_Scripts/AdminSystems/DataBuilders/CharactersBuilder.cs
+++ Assets/_Scripts/AdminSystems/DataBuilders/CharactersBuilder.cs
@@ -41,47 +41,55 @@ public class CharactersBuilder : BaseDataBuilder
 
         CreateCharacter("hio", "Hio", "Hio");
         SetDescription("Dueño de una pastelería y protagonista en esta historia.");
-        AddRandomDialogue(new List<string>() { "Hio frase", "frase1Continuacion" });
+        AddRandomDialogue(new List<string>() { "¿No se suponía que yo era mudo?" });
         FinishCharacter();
 
         CreateCharacter("nu", "Nu", "Nu Nu");
         SetDescription("Ángel de las Galletas.");
-        AddRandomDialogue(new List<string>() { "Nu frase", "frase1Continuacion" });
+        AddRandomDialogue(new List<string>() { "Ocurra lo que ocurra, jamás confíes en Evith." });
+        AddRandomDialogue(new List<string>() { "Este pueblo parece un buen sitio para vivir. Si no fuera por la miriada de problemas." });
         FinishCharacter();
 
         CreateCharacter("evith", "Evith", "Evith");
         SetDescription("Demonio de las Galletas.");
-        AddRandomDialogue(new List<string>() { "Evith frase", "frase1Continuacion" });
+        AddRandomDialogue(new List<string>() { "Nu intentará lavarte la cabeza con sus sermones baratos." });
+        AddRandomDialogue(new List<string>() { "Algún día seré reconocida como la reina de este mundo." });
         FinishCharacter();
 
         CreateCharacter("meri", "Meri la Leches", "Meri");
         SetDescription("Ganadera entusiasta de las vacas.");
-        AddRandomDialogue(new List<string>() { "Leches frase", "frase1Continuacion" });
+        AddRandomDialogue(new List<string>() { "Las vacas son la alegría de mi vida. Será mejor que nadie se meta con ellas." });
+        AddRandomDialogue(new List<string>() { "¡No estoy obsesionada con las vacas! También me gustan los becerros y los bueyes.", "¡¿Por qué me miras así de raro?! ¿Son distintos, no?" });
         FinishCharacter();
 
         CreateCharacter("mayor", "Alcalde, Antonio", "Alcalde");
         SetDescription("El muy tacaño alcalde del pueblo.");
-        AddRandomDialogue(new List<string>() { "Alcalde frase", "frase1Continuacion" });
+        AddRandomDialogue(new List<string>() { "A este pueblo lo que le falta son más bancos donde sentarse." });
+        AddRandomDialogue(new List<string>() { "¿El puerto? ¡Va de maravilla! Quizás el próximo lustro dispondremos de alguna barca." });
         FinishCharacter();
 
         CreateCharacter("miss_chocolate", "Miss Chocolate", "Ms.Chocolate");
         SetDescription("Excéntrica chocolatera.");
-        AddRandomDialogue(new List<string>() { "Miss frase", "frase1Continuacion" });
+        AddRandomDialogue(new List<string>() { "El chocolate es el alimento más intenso que existe." });
+        AddRandomDialogue(new List<string>() { "Dedico todo mi ser a buscar nuevas formas de crear chocolate." });
         FinishCharacter();
 
         CreateCharacter("canela", "Canela N Rama", "Cane");
         SetDescription("Coleccionista de artefactos antiguos.");
-        AddRandomDialogue(new List<string>() { "Rama frase", "frase1Continuacion" });
+        AddRandomDialogue(new List<string>() { "Mi colección se ha vuelto tan famosa que mucha gente viene de fuera para verla." });
+        AddRandomDialogue(new List<string>() { "Este pueblo no tiene nada de nada. Pero al menos se puede vivir en tranquilidad... a veces." });
         FinishCharacter();
 
         CreateCharacter("johny_setas", "Johny, el setas", "Johny");
         SetDescription("Un alquimista muy peculiar.");
-        AddRandomDialogue(new List<string>() { "setas fr", "frase1Continuacion" });
+        AddRandomDialogue(new List<string>() { "¡Esta pastelería que tienes es la leche, colega!" });
+        AddRandomDialogue(new List<string>() { "Me gusta todo lo que tenga que ver con los hongos.", "Pero no me importaría dedicarme a la panadería algún día." });
         FinishCharacter();
 
         CreateCharacter("mantecas", "Juanjo, el mantecas", "Juanjo");
         SetDescription("El agricultor malhumorado");
-        AddRandomDialogue(new List<string>() { "Juanjo frase", "frase1Continuacion" });
+        AddRandomDialogue(new List<string>() { "Mis hortalizas son las mejores del mundo entero.", "¡Será por eso que no paran de molestarme las plagas!" });
+        AddRandomDialogue(new List<string>() { "¡No estoy siempre de mal humor! ¡De hecho hoy estoy feliz! ¡¿No se me nota en la cara?!" });
         FinishCharacter();
     }
 
