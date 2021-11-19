8bf0641245c299a2d6b5c30b953b18e0c6ebedfe
jdrojo2018 <drojomartin@gmail.com>
2021-11-19 18:17:24 +0100

    Added base introduction sequence script

---
+++ Assets/_Scripts/AdminSystems/Systems/TutorialSystem/FirstDayIntroductionSequenceScript.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstDayIntroductionSequenceScript : MonoBehaviour
+{
+    private Event<ShowDialogueEvtArgs> _showDialogueCmd;
+
+    public void OnEnable()
+    {
+        var evtSys = Admin.Global.EventSystem;
+        _showDialogueCmd = evtSys.GetCommandByName<Event<ShowDialogueEvtArgs>>("dialogue_sys", "show_dialogue");
+    }
+
+    public void StartIntroductionSequence()
+    {
+        //Dentro pantalla en negro
+
+
+        _showDialogueCmd.Invoke(new ShowDialogueEvtArgs(new List<string>() {
+            "En un pequeño pueblo, un joven ha abierto una pastelería.",
+            "El pueblo ha visto días mejores. Los problemas surgen por doquier.",
+            "El joven escucha impotente, día tras día, los infortunios de sus clientes.",
+            "Hasta que un día...",
+            "'¡Quiero poder hacer algo al respecto!' Deseó el joven.",
+            "Lo que no sabía en ese momento era que su deseo iba a ser concedido ese mismo día."},
+            new ID("narrator"),
+               () => { NarratorDialogFinishedCallback(); }));
+    }
+
+    private void NarratorDialogFinishedCallback()
+    {
+        //Fuera pantalla en negro
+
+    }
+}
