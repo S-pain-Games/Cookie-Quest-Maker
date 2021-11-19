a0cb53696a418dc3df71df48862647b0d5f58fcc
Anthonys-source <62327779+Anthonys-source@users.noreply.github.com>
2021-11-19 18:46:50 +0100

    Fix Movement Stuck after tutorial

--- Assets/_Scripts/AdminSystems/Systems/UISystem/UISystem.cs
+++ Assets/_Scripts/AdminSystems/Systems/UISystem/UISystem.cs
@@ -33,7 +33,7 @@ namespace CQM.Systems
             commands.AddEvent(new ID("toggle_quest_making")).OnInvoked += () => ToggleUIBlockingMovement(_references.m_QuestMakingUi);
             commands.AddEvent(new ID("toggle_town")).OnInvoked += () => ToggleUIBlockingMovement(_references.m_TownUi);
             commands.AddEvent(new ID("toggle_settings")).OnInvoked += () => ToggleUIBlockingMovement(_references.m_SettingsGameplayUi);
-            commands.AddEvent(new ID("toggle_gameplay")).OnInvoked += () => ToggleUIBlockingMovement(_references.m_GameplayUi);
+            commands.AddEvent(new ID("toggle_gameplay")).OnInvoked += () => ToggleUI(_references.m_GameplayUi);
         }
 
         private void ToggleUI(GameObject ui)
