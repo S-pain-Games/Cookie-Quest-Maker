c34ae946b29e103fdeb37e3502b2f087a2319c69
jdrojo2018 <drojomartin@gmail.com>
2021-11-21 12:13:09 +0100

    Added missing repercusion to TownBuilder and loaded new data

--- Assets/_Scripts/AdminSystems/DataBuilders/TownBuilder.cs
+++ Assets/_Scripts/AdminSystems/DataBuilders/TownBuilder.cs
@@ -15,53 +15,98 @@ public class TownBuilder : BaseDataBuilder
     {
         m_Locations.Clear();
 
+        //MAYOR
         CreateLocation("mayor_home", "mayor", "Casa de Antonio", "description");
         AddRepercusion("wolves_gone");
         AddRepercusion("wolves_stay");
+
         AddRepercusion("mayor_assaulted");
         AddRepercusion("mayor_waste");
         AddRepercusion("mayor_robbed");
+
+        AddRepercusion("mayor_alerted");
+        AddRepercusion("mayor_relaxed");
+        AddRepercusion("mayor_ignore");
         FinishCreatingLocation();
 
+        //MERI
         CreateLocation("meri_rancho", "meri", "Rancho de Meri", "description");
         AddRepercusion("cows_harmed");
         AddRepercusion("cows_saved");
+
         AddRepercusion("cows_transformed");
         AddRepercusion("cows_back_to_normal");
         AddRepercusion("cows_and_goats_reversed");
+
+        AddRepercusion("wolves_victorious");
+        AddRepercusion("rats_victorious");
+        AddRepercusion("rats_mounting_wolves");
         FinishCreatingLocation();
 
+        //CANELA
         CreateLocation("canela_home", "canela", "Casa de Canela", "description");
         AddRepercusion("golden_egg_destroyed");
         AddRepercusion("golden_egg_safe");
         AddRepercusion("golden_egg_gone");
+
         AddRepercusion("artifacts_stolen");
         AddRepercusion("pendant_damaged");
         AddRepercusion("pendant_recovered");
         AddRepercusion("pendant_lost");
+
+        AddRepercusion("living_artifacts_destroyed");
+        AddRepercusion("living_artifacts_stopped");
+        AddRepercusion("living_artifacts_servants");
+
         FinishCreatingLocation();
 
+        //MS CHOCOLATE
         CreateLocation("chocolate_home", "miss_chocolate", "Casa de Miss Chocolate", "description");
         AddRepercusion("ms_chocolate_sabotaged");
         AddRepercusion("ms_chocolate_gunpowder");
         AddRepercusion("ms_chocolate_pepper");
+
         AddRepercusion("cows_poisoned");
         AddRepercusion("cows_experiment_success");
         AddRepercusion("cows_experiment_delayed");
+
+        AddRepercusion("ms_chocolate_experiment_canceled");
+        AddRepercusion("ms_chocolate_experiment_success");
+        AddRepercusion("ms_chocolate_great_success");
+
         FinishCreatingLocation();
 
+        //MANTECAS
         CreateLocation("mantecas_home", "mantecas", "Casa del Mantecas", "description");
         AddRepercusion("smart_rats_stay");
         AddRepercusion("smart_rats_gone");
         AddRepercusion("smart_rats_tribute");
+
         AddRepercusion("ducks_stay");
         AddRepercusion("ducks_gone");
         AddRepercusion("ducks_help");
+
+        AddRepercusion("mantecas_farm_damaged");
+        AddRepercusion("mantecas_farm_devastated");
+        AddRepercusion("mantecas_farm_revitalized");
+        AddRepercusion("mantecas_paranoic");
+
         FinishCreatingLocation();
 
+        //JOHNNY
         CreateLocation("johnny_home", "johny_setas", "Casa derJohnny", "description");
         AddRepercusion("competition_judges_stoned");
         AddRepercusion("competition_safe");
+
+        AddRepercusion("johnny_sabotaged");
+        AddRepercusion("johnny_enhanced");
+        AddRepercusion("johnny_neutral");
+
+        AddRepercusion("johnny_trance_interruptded");
+        AddRepercusion("johnny_transformed");
+        AddRepercusion("johnny_trance_achieved");
+        AddRepercusion("johnny_trance_convinced");
+
         FinishCreatingLocation();
     }
 
