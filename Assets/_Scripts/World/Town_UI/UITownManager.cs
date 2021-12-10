ff9a1937e3c0515a07790c58049f1eb1383d9596
jdrojo2018 <drojomartin@gmail.com>
2021-12-10 22:22:55 +0100

    Fixed global hapiness value on Town window

--- Assets/_Scripts/World/Town_UI/UITownManager.cs
+++ Assets/_Scripts/World/Town_UI/UITownManager.cs
@@ -1,7 +1,9 @@
 using CQM.Components;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace CQM.UI.Town
 {
@@ -10,12 +12,35 @@ namespace CQM.UI.Town
         [SerializeField] private List<TownBuildingBehaviour> m_Buildings = new List<TownBuildingBehaviour>();
         [SerializeField] private HappinessMeterAnimations m_currentMeter;
 
+        [SerializeField] private TextMeshProUGUI globalHapinessText;
+        private int baseHapinessValue = 100;    //50%
+        //Mínimo 0, Máximo 200
+ 
         public void OnEnable()
         {
             for (int i = 0; i < m_Buildings.Count; i++)
             {
                 m_Buildings[i].Initialize(this);
             }
+
+            CalculateGlobalHapiness();
+        }
+
+        private void CalculateGlobalHapiness()
+        {
+            int totalHapinessModifier = baseHapinessValue;
+
+            for (int i = 0; i < m_Buildings.Count; i++)
+            {
+                var locationComponent = Admin.Global.Components.GetComponentContainer<LocationComponent>().GetComponentByID(m_Buildings[i].BuildingID);
+                Debug.Log(locationComponent.m_LocName + " hapiness: " + locationComponent.m_Happiness);
+
+                totalHapinessModifier += locationComponent.m_Happiness;
+            }
+
+            int hapinessPercent = totalHapinessModifier * 50 / 100;
+
+            globalHapinessText.text = "FELICIDAD "+ hapinessPercent+"%";
         }
 
         public void SelectTownBuilding(TownBuildingBehaviour building)
@@ -23,6 +48,7 @@ namespace CQM.UI.Town
             // We could definitely cache this
             var locationComponent = Admin.Global.Components.GetComponentContainer<LocationComponent>().GetComponentByID(building.BuildingID);
             m_currentMeter.EnableForBuilding(building.transform.position, locationComponent.m_Happiness, locationComponent.m_LocName);
+
         }
 
         public void UnselectTownBuilding(TownBuildingBehaviour building)
